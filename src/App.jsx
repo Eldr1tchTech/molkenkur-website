d9179e5842d827fa86a9590d0cf0300c747b7b10
Eldr1tchTech <eldr1tchtech.dev@gmail.com>
2024-07-26 21:44:47 +0200

    maybe fix?

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
-import { BrowserRouter, Routes, Route, Router } from 'react-router-dom'
-import Hochzeiten from './pages/Hochzeiten.jsx'
-import Hotel from './pages/Hotel.jsx'
-import Restaurant from './pages/Restaurant.jsx'
-import Tagungen from './pages/Tagungen.jsx'
-import Events from './pages/Events.jsx'
-import Home from './pages/Home.jsx'
-import Datenschutz from './pages/Datenschutz.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Hochzeiten from './pages/Hochzeiten.jsx';
+import Hotel from './pages/Hotel.jsx';
+import Restaurant from './pages/Restaurant.jsx';
+import Tagungen from './pages/Tagungen.jsx';
+import Events from './pages/Events.jsx';
+import Home from './pages/Home.jsx';
+import Datenschutz from './pages/Datenschutz.jsx';
 
 function App() {
   return (
     <div className='App'>
-      <Routes>
-        <Route path='/molkenkur-website/' element={<Home/>} />
-        <Route path='/molkenkur-website/hochzeiten' element={<Hochzeiten/>} />
-        <Route path='/molkenkur-website/hotel' element={<Hotel/>} />
-        <Route path='/molkenkur-website/restaurant' element={<Restaurant/>} />
-        <Route path='/molkenkur-website/tagungen' element={<Tagungen/>} />
-        <Route path='/molkenkur-website/events' element={<Events/>} />
-        <Route path='/molkenkur-website/datenschutz' element={<Datenschutz/>} />
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/molkenkur-website/' element={<Home />} />
+          <Route path='/molkenkur-website/hochzeiten' element={<Hochzeiten />} />
+          <Route path='/molkenkur-website/hotel' element={<Hotel />} />
+          <Route path='/molkenkur-website/restaurant' element={<Restaurant />} />
+          <Route path='/molkenkur-website/tagungen' element={<Tagungen />} />
+          <Route path='/molkenkur-website/events' element={<Events />} />
+          <Route path='/molkenkur-website/datenschutz' element={<Datenschutz />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
